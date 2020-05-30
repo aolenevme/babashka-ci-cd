2719c63117779102929736c30c55c966666107b2
Alexander Olenev <olenev.biz@gmail.com>
2020-05-30 21:00:46 +0300

    feat/validating-prs: Basic validation of all branches
    Validation of all PRs to every branch.
    
    Titles should match a regex and description should be empty.

---
+++ .github/actions/pr-title-description-validation/index.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+function isTitleValid(titleRegexInput) {
+    // eslint-disable-next-line security/detect-non-literal-regexp
+    const titleRegex = new RegExp(titleRegexInput, "u");
+    const title
+        = github.context.payload
+        && github.context.payload.pull_request
+        && github.context.payload.pull_request.title;
+
+    return titleRegex.test(title);
+}
+
+function isTitleStartedWithRefName(refName) {
+    const title
+        = github.context.payload
+        && github.context.payload.pull_request
+        && github.context.payload.pull_request.title;
+
+    return typeof title === "string"
+        && typeof refName === "string"
+        && title.startsWith(refName);
+}
+
+function isDescriptionNotEmpty() {
+    const pullRequestDescription
+        = github.context.payload
+        && github.context.payload.pull_request
+        && github.context.payload.pull_request.body;
+
+    return typeof pullRequestDescription === "string"
+        && pullRequestDescription.length > 0;
+}
+
+function action() {
+    try {
+        const titleRegexInput = core.getInput("title-regex");
+
+        if (!isTitleValid(titleRegexInput)) {
+            core.setFailed(
+                // eslint-disable-next-line max-len
+                `The title of the pull request does not match regex pattern "${titleRegexInput}".`
+            );
+        }
+
+        const refName = github.context.payload.pull_request.head.ref;
+
+        if (!isTitleStartedWithRefName(refName)) {
+            core.setFailed(
+                `The title of the pull request has to start with ${refName}`
+            );
+        }
+
+        if (!isDescriptionNotEmpty()) {
+            core.setFailed(
+                `Pull request description is empty.`
+            );
+        }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+action();
