4b659b581fb71672b247e3b7127e3ff06d588375
eshekak <olenev.biz@gmail.com>
2020-06-02 23:33:49 +0300

    feat/rewrite-prepush-githook: Rewrote prepush githook into babashka

---
+++ ci/githooks-clj/prepush.clj
@@ -0,0 +1,24 @@
+#!/usr/bin/env bb
+
+(require '[clojure.java.shell :refer [sh]]
+         '[clojure.string :refer [trim]])
+
+(def branch-regex #"^(master|develop|(feat|release|hotfix)\/[a-z0-9._-]+)$")
+
+(defn get-local-branch-name []
+  (-> (sh "git" "rev-parse" "--abbrev-ref" "HEAD")
+      :out
+      (trim)))
+
+(defn valid-branch-name? []
+      (re-matches branch-regex (get-local-branch-name)))
+
+(defn print-error-msg []
+      (println (format "Branch names in this project must adhere to this contract: %s." branch-regex)))
+
+(defn pre-push []
+      (when-not (valid-branch-name?)
+                (print-error-msg)
+                (System/exit 1)))
+
+(pre-push)
