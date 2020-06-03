b81943e65858c2361bd05a3fd4d838f3de1bf11b
eshekak <olenev.biz@gmail.com>
2020-06-03 23:40:20 +0300

    feat/rewrite-precommit-githook-clj: Finished pre-commit.clj

--- ci/githooks-clj/pre-commit.clj
+++ ci/githooks-clj/pre-commit.clj
@@ -6,10 +6,11 @@
 (defn validate-branch-name [] (load-file "ci/githooks-clj/pre-push.clj"))
 
 (defn lint-clojure-code []
-      (-> (sh "clj-kondo" "--lint" "src")
-          :out
-          (trim)
-          (println)))
+      (let [lint-res (sh "clj-kondo" "--lint" "src" "ci")
+            exit (:exit lint-res)
+            out (:out lint-res)]
+           (println out)
+           (when-not (or (= exit 2) (= exit 0)) (System/exit 1))))
 
 (validate-branch-name)
 (lint-clojure-code)
