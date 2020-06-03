e563bcadeda974c9caeef4bf5d12cef5eabcc10a
eshekak <olenev.biz@gmail.com>
2020-06-03 23:05:06 +0300

    feat/rewrite-precommit-githook-clj: Migrated to clj-kondo

--- ci/githooks-clj/pre-commit.clj
+++ ci/githooks-clj/pre-commit.clj
@@ -1,5 +1,15 @@
 #!/usr/bin/env bb
 
+(require '[clojure.java.shell :refer [sh]]
+         '[clojure.string :refer [trim]])
+
 (defn validate-branch-name [] (load-file "ci/githooks-clj/pre-push.clj"))
 
+(defn lint-clojure-code []
+      (-> (sh "clj-kondo" "--lint" "src")
+          :out
+          (trim)
+          (println)))
+
 (validate-branch-name)
+(lint-clojure-code)
