b102824866881a0094983a0348b52e0f7eebeeeb
eshekak <olenev.biz@gmail.com>
2020-06-04 15:15:14 +0300

    feat/rewrite-title-description-validation-action: Test action x26

--- .github/actions/pr-title-description-validation/index.clj
+++ .github/actions/pr-title-description-validation/index.clj
@@ -16,15 +16,14 @@
       (let [description-length (count description)]
            (> description-length 0)))
 
-(defn action []
-      ((when-not (valid-title?)
-                 ((println "The title of the pull request does not match regex pattern: " title-regex)
-                  (System/exit 1)))
-       (when-not (title-starts-with-ref-name?)
-                 ((println "The title of the pull request has to start with " ref-name)
-                  (System/exit 1)))
-       (when-not (is-description-not-empty?)
-                 ((println "Pull request description is empty.")
-                  (System/exit 1)))))
+(when-not (valid-title?)
+          (do (println "The title of the pull request does not match regex pattern: " title-regex)
+              (System/exit 1)))
 
-(action)
+(when-not (title-starts-with-ref-name?)
+          (do (println "The title of the pull request has to start with " ref-name)
+              (System/exit 1)))
+
+(when-not (is-description-not-empty?)
+          (do (println "Pull request description is empty.")
+              (System/exit 1)))
