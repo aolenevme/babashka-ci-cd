a7a281a6d29b265255582fb04170e886f7c0f06a
eshekak <olenev.biz@gmail.com>
2020-06-04 14:30:59 +0300

    feat/rewrite-title-description-validation-action: Test action x19

--- .github/actions/pr-title-description-validation/index.clj
+++ .github/actions/pr-title-description-validation/index.clj
@@ -1,7 +1,7 @@
 #!/usr/bin/env bb
 
 (require '[clojure.java.shell :refer [sh]]
-         '[clojure.string :as s])
+         '[clojure.string :as str])
 
 (def title-regex (re-pattern (first *command-line-args*)))
 (def title (second *command-line-args*))
@@ -9,7 +9,7 @@
 
 (defn valid-title? [] (re-matches title-regex title))
 
-(defn title-starts-with-ref-name? [] (s/starts-with? title ref-name))
+(defn title-starts-with-ref-name? [] (str/starts-with? title ref-name))
 
 (valid-title?)
 (title-starts-with-ref-name?)
