4d21cbeec1231a0859e21ab1adf35f9a67183d0b
eshekak <olenev.biz@gmail.com>
2020-06-04 14:48:08 +0300

    feat/rewrite-title-description-validation-action: Test action x24

--- .github/actions/pr-title-description-validation/index.clj
+++ .github/actions/pr-title-description-validation/index.clj
@@ -4,12 +4,18 @@
          '[clojure.string :as str])
 
 (def title-regex (re-pattern (first *command-line-args*)))
-(def title (second *command-line-args*))
-(def ref-name (second (next *command-line-args*)))
+(def ref-name (second *command-line-args*))
+(def title (second (next *command-line-args*)))
+(def description (second (next (next *command-line-args*))))
 
 (defn valid-title? [] (re-matches title-regex title))
 
 (defn title-starts-with-ref-name? [] (str/starts-with? title ref-name))
 
+(defn is-description-not-empty? []
+      (let [description-length (count description)]
+           (> description-length 0)))
+
 (valid-title?)
 (title-starts-with-ref-name?)
+(is-description-not-empty?)
