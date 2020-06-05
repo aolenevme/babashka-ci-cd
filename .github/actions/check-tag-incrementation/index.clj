7a82f7790e4128fc345e0b75d449749723a6cf63
eshekak <olenev.biz@gmail.com>
2020-06-05 10:54:16 +0300

    feat/tag-increasing-check: Test x18

--- .github/actions/check-tag-incrementation/index.clj
+++ .github/actions/check-tag-incrementation/index.clj
@@ -3,6 +3,10 @@
 (require '[clojure.java.shell :refer [sh]]
          '[clojure.string :refer [trim]])
 
+(def package-json-diff (first *command-line-args*))
+
+(println package-json-diff)
+
 ;(-> (sh "git checkout master")
 ;    :out
 ;    (trim)
