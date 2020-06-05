33897c67b68215fcca2510d9e9182e6a543be6a9
Alexander Olenev <olenev.biz@gmail.com>
2020-06-06 01:55:31 +0300

    hotfix/rlease-v0.2.1: Merge release back to develop
    Handle try/catch case when version wasn`t changed

--- .github/actions/check-package-version-incrementation/index.clj
+++ .github/actions/check-package-version-incrementation/index.clj
@@ -6,23 +6,22 @@
 (def package-json-diff (first *command-line-args*))
 (def version-substr-length 12)
 
-(def old-version-substr-begin (+ (s/index-of package-json-diff "-  \"version\": \"") version-substr-length))
-(def new-version-substr-begin (+ (s/index-of package-json-diff "+  \"version\": \"") version-substr-length))
-
-(def old-version-comma-end (s/index-of package-json-diff "," old-version-substr-begin))
-(def new-version-comma-end (s/index-of package-json-diff "," new-version-substr-begin))
-
-(def old-version (subs package-json-diff old-version-substr-begin old-version-comma-end))
-(def new-version (subs package-json-diff new-version-substr-begin new-version-comma-end))
-
-(def splitted-old-version (s/split old-version #"\."))
-(def splitted-new-version (s/split new-version #"\."))
-
-(def is-version-incremented? (= (compare splitted-new-version splitted-old-version) 1))
+(defn is-version-incremented? []
+      (try
+        (let [old-version-substr-begin (+ (s/index-of package-json-diff "-  \"version\": \"") version-substr-length)
+              new-version-substr-begin (+ (s/index-of package-json-diff "+  \"version\": \"") version-substr-length)
+              old-version-comma-end (s/index-of package-json-diff "," old-version-substr-begin)
+              new-version-comma-end (s/index-of package-json-diff "," new-version-substr-begin)
+              old-version (subs package-json-diff old-version-substr-begin old-version-comma-end)
+              new-version (subs package-json-diff new-version-substr-begin new-version-comma-end)
+              splitted-old-version (s/split old-version #"\.")
+              splitted-new-version (s/split new-version #"\.")]
+             (= (compare splitted-new-version splitted-old-version) 1))
+        (catch Exception _ false)))
 
 (defn print-error-msg []
-      (println (format "Please, increment version in package.json before the merging. Current version is %s." old-version )))
+      (println "Please, increment version in package.json before the merging."))
 
-(when-not is-version-incremented?
+(when-not (is-version-incremented?)
           (print-error-msg)
           (System/exit 1))
