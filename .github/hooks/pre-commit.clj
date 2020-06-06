00f66c51101488f5a858bddd087485e166f5930a
eshekak <olenev.biz@gmail.com>
2020-06-07 00:00:45 +0300

    feat/add-auto-formatting: Check format-code in pre-commit hook

--- .github/hooks/pre-commit.clj
+++ .github/hooks/pre-commit.clj
@@ -5,12 +5,30 @@
 
 (defn validate-branch-name [] (load-file ".github/hooks/pre-push.clj"))
 
-(defn lint-clojure-code []
-      (let [lint-res (sh "clj-kondo" "--lint" "src" ".github")
+(defn lint-code []
+      (let [lint-res (sh "clj-kondo" "--lint" "src")
             exit (:exit lint-res)
             out (:out lint-res)]
            (println out)
            (when-not (or (= exit 2) (= exit 0)) (System/exit 1))))
 
+(defn format-code []
+      (let [
+            ;format-res (sh "clj -Sdeps '{:deps {mvxcvi/cljstyle {:git/url \"https://github.com/greglook/cljstyle.git\", :sha \"c8bc620aeadd022136bb333970c03edf41627417\"}}}' -m cljstyle.main fix ../../src \n")
+            format-res (sh "pwd")
+            git-add-res (sh "pwd")
+            ;git-add-res (sh "git add .")
+            format-exit (:exit format-res)
+            git-add-exit (:exit git-add-res)
+            format-out (:out format-res)
+            git-add-out (:out format-res)]
+           (println format-out)
+           (println git-add-out)
+           (when-not (and
+                       (or (= format-exit 2) (= format-exit 0))
+                       (or (= git-add-exit 2) (= git-add-exit 0)))
+                     (System/exit 1))))
+
 (validate-branch-name)
-(lint-clojure-code)
+(format-code)
+(lint-code)
