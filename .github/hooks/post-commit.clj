56f2de2c0adf1d12abb9ba061480c5a73cbccb3e
eshekak <olenev.biz@gmail.com>
2020-06-07 22:51:53 +0300

    release/v0.2.4: Test x2

--- .github/hooks/post-commit.clj
+++ .github/hooks/post-commit.clj
@@ -1,26 +1,38 @@
 #!/usr/bin/env bb
 
 (require '[clojure.java.shell :refer [sh]]
-         '[clojure.string :refer [trim]])
+         '[clojure.string :as s])
+
+(def version-substr-length 12)
 
 (defn get-top-level-directory []
       (-> (sh "git" "rev-parse" "--show-toplevel")
           :out
-          (trim)))
+          (s/trim)))
 
 (defn path->package-json []
       (str (get-top-level-directory) "/package.json"))
 
 
 (defn get-version []
-      (-> (sh "git" "diff" "HEAD^..HEAD" "--" (path->package-json) "|" "grep" "'^\\+.*version'" "|" "sed" "-s" "'s/[^0-9\\.]//g'")
+      (-> (sh "git" "diff" "HEAD^..HEAD" "--" (path->package-json))
           :out
-          (trim)))
+          (s/trim)))
 
-(try
-  (when-let [version (get-version)
-             is-version-increased? (not= (count version) 0)]
-            (sh "git" "tag" "-a" (str "v" version) "-m" version))
-  (catch Exception e
-    (println e)
-    (System/exit 1)))
+(defn is-version-incremented? []
+      (try
+        (let [new-version-substr-begin (+ (s/index-of (get-version) "+  \"version\": \"") version-substr-length)
+              new-version-comma-end (s/index-of package-json-diff "," new-version-substr-begin)
+              new-version (subs package-json-diff new-version-substr-begin new-version-comma-end)]
+             new-version)
+        (catch Exception _ "")))
+
+(println (is-version-incremented?))
+
+;(try
+;  (when-let [version (get-version)
+;             is-version-increased? (not= (count version) 0)]
+;            (sh "git" "tag" "-a" (str "v" version) "-m" version))
+;  (catch Exception e
+;    (println e)
+;    (System/exit 1)))
