86826003506ecd0ce31c373720f84b2d139dee45
eshekak <olenev.biz@gmail.com>
2020-06-07 23:03:56 +0300

    release/v0.2.4: Test x14

--- .github/hooks/post-commit.clj
+++ .github/hooks/post-commit.clj
@@ -33,7 +33,6 @@
              is-version-increased? (not= (count version) 0)]
             (-> (sh "git" "tag" "-a" (str "v" version) "-m" version)
                 :out
-                (s/trim))
-            (println version))
+                (s/trim)))
   (catch Exception _
     (System/exit 1)))
