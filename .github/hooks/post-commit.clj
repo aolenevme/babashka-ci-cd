182e0ff250389bafe750dbf41d2336876925c7f2
eshekak <olenev.biz@gmail.com>
2020-06-07 23:03:07 +0300

    release/v0.2.4: Test x12

--- .github/hooks/post-commit.clj
+++ .github/hooks/post-commit.clj
@@ -31,9 +31,9 @@
 (try
   (when-let [version (get-version)
              is-version-increased? (not= (count version) 0)]
-            (-> (sh "git" "tag" "-a" (str "v" version) "-m" version)
+            (do (-> (sh "git" "tag" "-a" (str "v" version) "-m" version)
                 :out
                 (s/trim))
-            (println version is-version-increased?))
+            (println version is-version-increased?)))
   (catch Exception _
     (System/exit 1)))
