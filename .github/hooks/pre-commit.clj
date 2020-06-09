a60899ae67729968b8b585eb9895b39fe04e003e
eshekak <olenev.biz@gmail.com>
2020-06-09 20:42:43 +0300

    feat/add-kibit-linting: Added kibit via Leinigen

--- .github/hooks/pre-commit.clj
+++ .github/hooks/pre-commit.clj
@@ -20,8 +20,8 @@
             git-add-out (:out format-res)]
            (println format-out git-add-out)
            (when-not (and
-                       (or (= format-exit 2) (= format-exit 0))
-                       (or (= git-add-exit 2) (= git-add-exit 0)))
+                       (or (= format-exit 2) (zero? format-exit))
+                       (or (= git-add-exit 2) (zero? git-add-exit)))
                      (System/exit 1))))
 
 (defn lint-code []
@@ -29,7 +29,7 @@
             exit (:exit lint-res)
             out (:out lint-res)]
            (println out)
-           (when-not (or (= exit 2) (= exit 0)) (System/exit 1))))
+           (when-not (or (= exit 2) (zero? exit)) (System/exit 1))))
 
 (try (do (validate-branch-name)
          (format-code)
