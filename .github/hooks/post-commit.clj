2392e3af37f235b172286dfc339baae312a163a3
Alexander Olenev <olenev.biz@gmail.com>
2020-06-09 20:02:15 +0300

    release/v0.2.4: Merge release to develop
    Merge release to develop

--- .github/hooks/post-commit.clj
+++ .github/hooks/post-commit.clj
@@ -1,26 +1,40 @@
 #!/usr/bin/env bb
 
 (require '[clojure.java.shell :refer [sh]]
-         '[clojure.string :refer [trim]])
+         '[clojure.string :as s])
+
 
 (defn get-top-level-directory []
       (-> (sh "git" "rev-parse" "--show-toplevel")
           :out
-          (trim)))
+          (s/trim)))
 
 (defn path->package-json []
       (str (get-top-level-directory) "/package.json"))
 
 
-(defn get-version []
-      (-> (sh "git" "diff" "HEAD^..HEAD" "--" (path->package-json) "|" "grep" "'^\\+.*version'" "|" "sed" "-s" "'s/[^0-9\\.]//g'")
+(defn get-package-json-diff []
+      (-> (sh "git" "diff" "HEAD^..HEAD" "--" (path->package-json))
           :out
-          (trim)))
+          (s/trim)))
+
+(def version-substr-length 15)
+
+(defn get-version []
+      (try
+        (let [new-version-substr-begin (+ (s/index-of (get-package-json-diff) "+  \"version\": \"") version-substr-length)
+              new-version-comma-end (s/index-of (get-package-json-diff) "\"," new-version-substr-begin)
+              new-version (subs (get-package-json-diff) new-version-substr-begin new-version-comma-end)]
+             new-version)
+        (catch Exception _ "")))
 
 (try
   (when-let [version (get-version)
              is-version-increased? (not= (count version) 0)]
-            (sh "git" "tag" "-a" (str "v" version) "-m" version))
+            (-> (sh "git" "tag" "-a" (str "v" version) "-m" version)
+                :out
+                (s/trim)
+                (println)))
   (catch Exception e
     (println e)
     (System/exit 1)))
