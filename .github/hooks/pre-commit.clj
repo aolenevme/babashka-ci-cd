98f8c6c9503d77e9fce0a51af1f28eadae913e00
Alexander Olenev <olenev.biz@gmail.com>
2020-06-09 20:49:35 +0300

    feat/add-kibit-linting: There is function for this
    Use https://github.com/jonase/kibit to master your Clojure skills!

--- .github/hooks/pre-commit.clj
+++ .github/hooks/pre-commit.clj
@@ -20,8 +20,8 @@
             git-add-out (:out format-res)]
            (println format-out git-add-out)
            (when-not (and
-                       (or (= format-exit 2) (= format-exit 0))
-                       (or (= git-add-exit 2) (= git-add-exit 0)))
+                       (or (= format-exit 2) (zero? format-exit))
+                       (or (= git-add-exit 2) (zero? git-add-exit)))
                      (System/exit 1))))
 
 (defn lint-code []
@@ -29,11 +29,18 @@
             exit (:exit lint-res)
             out (:out lint-res)]
            (println out)
-           (when-not (or (= exit 2) (= exit 0)) (System/exit 1))))
+           (when-not (or (= exit 2) (zero? exit)) (System/exit 1))))
+
+(defn kibit-lint []
+      (let [lint-res (sh "lein" "kibit")
+            exit (:exit lint-res)
+            out (:out lint-res)]
+           (println out)
+           (when-not (zero? exit) (System/exit 1))))
 
 (try (do (validate-branch-name)
          (format-code)
-         (lint-code))
-     (catch Exception e
-       (println e)
+         (lint-code)
+         (kibit-lint))
+     (catch Exception _
        (System/exit 1)))
