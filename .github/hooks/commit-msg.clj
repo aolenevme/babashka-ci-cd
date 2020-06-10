c9c92552d03b0cb843aa2184fabace965996f598
Alexander Olenev <olenev.biz@gmail.com>
2020-06-10 22:46:50 +0300

    feat/add-tests-hooks-and-actions: Improve linting, formatting, code hints
    Now clj-kondo, cljstyle and kibit runs on all .clj scripts including CI/CD scripts

--- .github/hooks/commit-msg.clj
+++ .github/hooks/commit-msg.clj
@@ -1,32 +1,38 @@
-#!/usr/bin/env bb
+(defn- get-current-branch-name
+  []
+  (-> (shell/sh "git" "rev-parse" "--abbrev-ref" "HEAD")
+      :out
+      (str/trim)))
 
-(require '[clojure.java.shell :refer [sh]]
-         '[clojure.string :refer [trim]]
-         '[clojure.string :as s])
 
-(defn get-current-branch-name []
-      (-> (sh "git" "rev-parse" "--abbrev-ref" "HEAD")
-          :out
-          (trim)))
+(defn- create-commit-msg-regex
+  []
+  (re-pattern (str "^" (str/replace-first (get-current-branch-name) #"\/" "\\\\/") ":{1} .{1,}$")))
 
-(defn create-commit-msg-regex []
-      (re-pattern (str "^" (s/replace-first (get-current-branch-name) #"\/" "\\\\/") ":{1} .{1,}$")))
 
-(defn get-current-commit-msg []
-      (-> (sh "cat" ".git/COMMIT_EDITMSG")
-          :out
-          (trim)))
+(defn- get-current-commit-msg
+  []
+  (-> (shell/sh "cat" ".git/COMMIT_EDITMSG")
+      :out
+      (str/trim)))
 
-(defn valid-commit-msg? []
-      (re-matches (create-commit-msg-regex) (get-current-commit-msg)))
 
-(defn print-error-msg []
-      (println "Your commit message must follow this regex:")
-      (println (create-commit-msg-regex)))
+(defn- valid-commit-msg?
+  []
+  (re-matches (create-commit-msg-regex) (get-current-commit-msg)))
 
-(defn commit-msg []
-      (when-not (valid-commit-msg?)
-                (print-error-msg)
-                (System/exit 1)))
+
+(defn- print-error-msg
+  []
+  (println "Your commit message must follow this regex:")
+  (println (create-commit-msg-regex)))
+
+
+(defn- commit-msg
+  []
+  (when-not (valid-commit-msg?)
+    (print-error-msg)
+    (System/exit 1)))
+
 
 (commit-msg)
