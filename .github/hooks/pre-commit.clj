483af3eb700ccb87dc0df6b26b5a73ee2c8d9fd0
eshekak <olenev.biz@gmail.com>
2020-06-17 20:11:45 +0300

    feat/run-linting-on-push-or-pr: Moved branch name validation into a separate script

--- .github/hooks/pre-commit.clj
+++ .github/hooks/pre-commit.clj
@@ -1,26 +1,6 @@
-(defn- validate-branch-name
-  []
-  (load-file ".github/hooks/pre-push.clj"))
-
-
-(defn- format-code
-  []
-  (load-file ".github/common-scripts/format-code.clj"))
-
-
-(defn- lint-code
-  []
-  (load-file ".github/common-scripts/lint-code.clj"))
-
-
-(defn- kibit-lint
-  []
-  (load-file ".github/common-scripts/kibit-lint.clj"))
-
-
-(try (validate-branch-name)
-     (format-code)
-     (lint-code)
-     (kibit-lint)
+(try (load-file ".github/common-scripts/validate-branch-name.clj")
+     (load-file ".github/common-scripts/format-code.clj")
+     (load-file ".github/common-scripts/lint-code.clj")
+     (load-file ".github/common-scripts/kibit-lint.clj")
      (catch Exception _
        (System/exit 1)))
