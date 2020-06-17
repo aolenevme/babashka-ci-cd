f1e2e71dfb148a85a5e74f35143af47cd8026fcd
eshekak <olenev.biz@gmail.com>
2020-06-17 20:29:25 +0300

    feat/run-linting-on-push-or-pr: Added code quality check on every push and renamed the whole CI/CD :)

---
+++ .github/actions/code-quality-check/index.clj
@@ -0,0 +1,5 @@
+(try (load-file ".github/common-scripts/validate-branch-name.clj")
+     (load-file ".github/common-scripts/lint-code.clj")
+     (load-file ".github/common-scripts/kibit-lint.clj")
+     (catch Exception _
+       (System/exit 1)))
