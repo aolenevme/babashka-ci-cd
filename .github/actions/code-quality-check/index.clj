49281c5fa8d3abe235a209adcf043d6748fd98dc
eshekak <olenev.biz@gmail.com>
2020-06-17 23:08:52 +0300

    feat/run-linting-on-push-or-pr: Messing with Dockerfile context x13

--- .github/actions/code-quality-check/index.clj
+++ .github/actions/code-quality-check/index.clj
@@ -1,4 +1,5 @@
-(try (load-file ".github/common-scripts/lint-code.clj")
-     (load-file ".github/common-scripts/kibit-lint.clj")
-     (catch Exception _
-       (System/exit 1)))
+(def ^:private lint-code-file-content (first *command-line-args*))
+(def ^:private kibit-lint-file-content (second *command-line-args*))
+
+(shell/sh "bb" lint-code-file-content)
+(shell/sh "bb" kibit-lint-file-content)
