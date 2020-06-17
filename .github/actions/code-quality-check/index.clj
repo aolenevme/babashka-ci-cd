b5037b93e0289c1fb8d7d2593d80bdcb9d93a097
eshekak <olenev.biz@gmail.com>
2020-06-17 21:13:59 +0300

    feat/run-linting-on-push-or-pr: Messing with Dockerfile context x5

--- .github/actions/code-quality-check/index.clj
+++ .github/actions/code-quality-check/index.clj
@@ -1,5 +1,4 @@
-(try (load-file ".github/common-scripts/validate-branch-name.clj")
-     (load-file ".github/common-scripts/lint-code.clj")
+(try (load-file ".github/common-scripts/lint-code.clj")
      (load-file ".github/common-scripts/kibit-lint.clj")
      (catch Exception _
        (System/exit 1)))
