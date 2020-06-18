71fd3552c3e4dd94df328134836d4f98f6b3eab8
Alexander Olenev <olenev.biz@gmail.com>
2020-06-19 01:04:04 +0300

    feat/run-linting-on-push-or-pr
    Now I run clj-kondo and kibit linting on every push. Plus, I have added self-hosted runner and removed all Docker actions. Now leinigen, clojure, java, babashka, clj-kondo are installed on the runner

--- .github/hooks/pre-commit.clj
+++ .github/hooks/pre-commit.clj
@@ -1,51 +1,6 @@
-(defn- validate-branch-name
-  []
-  (load-file ".github/hooks/pre-push.clj"))
-
-
-(defn- format-code
-  []
-  (let [format-res (shell/sh "clj"
-                             "-Sdeps"
-                             "{:deps {mvxcvi/cljstyle {:git/url \"https://github.com/greglook/cljstyle.git\", :sha \"c8bc620aeadd022136bb333970c03edf41627417\"}}}"
-                             "-m"
-                             "cljstyle.main"
-                             "fix"
-                             "src"
-                             ".github")
-        git-add-res (shell/sh "git" "add" ".")
-        format-exit (:exit format-res)
-        git-add-exit (:exit git-add-res)
-        format-out (:out format-res)
-        git-add-out (:out format-res)]
-    (println format-out git-add-out)
-    (when-not (and
-                (or (= format-exit 2) (zero? format-exit))
-                (or (= git-add-exit 2) (zero? git-add-exit)))
-      (System/exit 1))))
-
-
-(defn- lint-code
-  []
-  (let [lint-res (shell/sh "clj-kondo" "--lint" "src" ".github")
-        exit (:exit lint-res)
-        out (:out lint-res)]
-    (println out)
-    (when-not (or (= exit 2) (zero? exit)) (System/exit 1))))
-
-
-(defn- kibit-lint
-  []
-  (let [lint-res (shell/sh "lein" "kibit" "src" ".github")
-        exit (:exit lint-res)
-        out (:out lint-res)]
-    (println out)
-    (when-not (zero? exit) (System/exit 1))))
-
-
-(try (validate-branch-name)
-     (format-code)
-     (lint-code)
-     (kibit-lint)
+(try (load-file ".github/common-scripts/validate-branch-name.clj")
+     (load-file ".github/common-scripts/format-code.clj")
+     (load-file ".github/common-scripts/lint-code.clj")
+     (load-file ".github/common-scripts/kibit-lint.clj")
      (catch Exception _
        (System/exit 1)))
