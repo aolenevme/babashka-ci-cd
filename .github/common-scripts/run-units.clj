de0d9d210ea858eaa0447ecdfc7c72f135263eb5
eshekak <olenev.biz@gmail.com>
2020-06-20 23:47:42 +0300

    feat/run-unit-tests-on-precommit: Check pre-commit hook with unit tests x2

--- .github/common-scripts/run-units.clj
+++ .github/common-scripts/run-units.clj
@@ -2,4 +2,4 @@
       exit (:exit test-res)
       out (:out test-res)]
   (println out)
-  (when-not (and (zero? exit) (not (str/includes? out "0 failures, 0 errors"))) (System/exit 1)))
+  (when-not (and (zero? exit) (str/includes? out "0 failures, 0 errors")) (System/exit 1)))
